43539b04efbf2342d23cf42770b4d07dcbc4c375
comp500 <comp500@users.noreply.github.com>
2018-02-06 21:58:06 +0000

    Finish adding fetch-favicon code

--- src/favicons.js
+++ src/favicons.js
@@ -1,4 +1,50 @@
 const faviconsSchema = require("./faviconsSchema.js");
+const xray = require("x-ray")();
+const resolveUrl = require("url").resolve;
+
+let downloadPage = (url) => {
+	return xray(url, faviconsSchema.selectors.join(), [{
+		href: '@href',
+		content: '@content',
+		property: '@property',
+		rel: '@rel',
+		name: '@name',
+		sizes: '@sizes',
+		title: 'title'
+	}]);
+};
+
+let getFavicon = (url) => {
+	return new Promise(function (resolve, reject) {
+		downloadPage(url)((err, favicons) => {
+			if (err) {
+				return reject(err);
+			}
+
+			favicons.push({
+				href: resolveUrl(url, 'favicon.ico'),
+				name: 'favicon.ico'
+			});
+
+			favicons = favicons.map((favicon) => {
+				const f = {
+					href: favicon.href || favicon.content,
+					name: favicon.name || favicon.rel || favicon.property,
+					size: Math.min.apply(null, (favicon.sizes || '').split(/[^0-9\.]+/g)) || undefined
+				};
+
+				if (!f.size) {
+					delete f.size;
+				}
+
+				return f;
+			})
+
+			//markActiveFavicon(favicons, size)
+			return resolve(favicons)
+		})
+	});
+};
 
 let count = 0;
 
@@ -6,7 +52,7 @@ module.exports = (faviconPath) => {
 	return (array) => {
 		return Promise.all(array.map((package) => {
 			if (package.homepage) {
-				return fetchFavicons(package.homepage).then((favicons) => {
+				return getFavicon(package.homepage).then((favicons) => {
 					package.favicons = favicons;
 					console.log(count++);
 					return package;
