1ce548ee0bba87577be1af9cb0b9ea046df433df
comp500 <comp500@users.noreply.github.com>
2018-02-07 01:13:53 +0000

    Load listener

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,17 +1,19 @@
-fetch("/list.json").then(function(response) {
-	return response.json();
-}).then(function(data) {
-	let test = "";
-	Object.keys(data).forEach(key => {
-		test += "<div class=\"package\">"
-		if (data[key].icon) {
-			test += "<img src=\"" + data[key].icon + "\" width=\"32\">"
-		}
-		test += "<strong>";
-		test += key;
-		test += "</strong> ";
-		test += data[key].friendlyName;
-		test += "</div>";
+window.addEventListener("load", function(event) {
+	fetch("/list.json").then(function(response) {
+		return response.json();
+	}).then(function(data) {
+		let test = "";
+		Object.keys(data).forEach(key => {
+			test += "<div class=\"package\">"
+			if (data[key].icon) {
+				test += "<img src=\"" + data[key].icon + "\" width=\"32\">"
+			}
+			test += "<strong>";
+			test += key;
+			test += "</strong> ";
+			test += data[key].friendlyName;
+			test += "</div>";
+		});
+		document.getElementById("packages").innerHTML = test;
 	});
-	document.getElementById("packages").innerHTML = test;
 });
